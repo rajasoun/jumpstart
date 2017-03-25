0fcde9d7beec5f511d20fbe2bf52912ea8d40643
Raja. Soundaramourty <rajasoun@icloud.com>
2017-03-25 20:13:35 +0530

    [US13686] Simplify - Refactor

--- run.sh
+++ run.sh
@@ -5,15 +5,6 @@ ANSIBLE_CMD="ansible-playbook -i ansible/inventory/topology  run.yml"
 ANSIBLE_VAULT_FILE=" --vault-password-file=./secrets/.vault_pass"
 LOG="> run.log 2>&1 &"
 
-ENV='vagrant-vm'
-roles=( base ntp java maven docker ansible mmonit monit )
-for role in "${roles[@]}"
-do
-  echo "Install $role"
-  CMD="$ANSIBLE_CMD -e role=$role -e hosts_to_run=$ENV  -e env=$ENV $ANSIBLE_VAULT_FILE"
-  $CMD
-done
-
 ENV='cloud-vm'
 roles=( base ntp java maven docker ansible mmonit monit )
 for role in "${roles[@]}"
