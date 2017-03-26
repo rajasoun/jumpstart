51f471651a1ef28161cc66635ca3e8b34cef84c0
Raja. Soundaramourty <rajasoun@icloud.com>
2017-03-27 00:46:54 +0530

    Ops Scripts

--- set-repo.sh
+++ set-repo.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
-
+mkdir -p ~/Workspace/ck/
+cd ~/Workspace/ck/
+git clone https://github.com/rajasoun/jumpstart
+cd jumpstart
 gem install overcommit
 overcommit --install
 overcommit --sign
